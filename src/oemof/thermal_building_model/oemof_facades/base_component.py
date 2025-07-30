911c7b1ab7b799bc9fd9630fa12679128c012808
hill_mx <maximilian.hillen@dlr.de>
2025-07-30 13:17:49 +0200

    delete base component multiperiod =True to False

--- src/oemof/thermal_building_model/oemof_facades/base_component.py
+++ src/oemof/thermal_building_model/oemof_facades/base_component.py
@@ -16,7 +16,7 @@ class BaseComponent:
 class TimeConfiguration:
     lifetime: float
     observation_period: float = 20
-    multiperiod: bool = True
+    multiperiod: bool = False
 
 @dataclass
 class InvestmentComponents(TimeConfiguration):
