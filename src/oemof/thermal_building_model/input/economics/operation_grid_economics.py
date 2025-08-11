7826e5092c1abf3dd28d392d2e02be9ca8b2b0d0
hill_mx <maximilian.hillen@dlr.de>
2025-08-11 16:33:00 +0200

    imrpoved co2 emission as average over 20 years

--- src/oemof/thermal_building_model/input/economics/operation_grid_economics.py
+++ src/oemof/thermal_building_model/input/economics/operation_grid_economics.py
@@ -5,14 +5,14 @@ gas_grid_config = GridComponents(
     working_rate=0.14 / 1000, #biogas
     revenue=0,
     price_change_factor=0.02,
-    co2_per_flow=0.137 / 1000)
+    co2_per_flow=0.1282 / 1000)
 
 # Gas Grid
 hydrogen_grid_config = GridComponents(
-    working_rate=0.18 / 1000, # https://www.sciencedirect.com/science/article/pii/S037877882200651X  https://www.sciencedirect.com/science/article/pii/S2211467X24001391
+    working_rate=0.22 / 1000, # https://www.sciencedirect.com/science/article/pii/S037877882200651X  https://www.sciencedirect.com/science/article/pii/S2211467X24001391
     revenue=0,
     price_change_factor=0.02,
-    co2_per_flow=0.056 / 1000)
+    co2_per_flow=0.0315 / 1000)
 
 
 # Electricity Grid
@@ -20,7 +20,7 @@ electricity_grid_config = GridComponents(
     working_rate=0.28 / 1000,
     revenue=0.0733 / 1000,
     price_change_factor=0.02,
-    co2_per_flow=0.260 / 1000)
+    co2_per_flow=0.0783 / 1000)
 
 
 # Heat Grid
