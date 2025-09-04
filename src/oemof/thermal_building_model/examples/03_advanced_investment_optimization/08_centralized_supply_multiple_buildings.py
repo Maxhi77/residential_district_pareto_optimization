4307a76e6200f79bd82362efc63414cab0c48f2b
hill_mx <maximilian.hillen@dlr.de>
2025-09-04 11:52:43 +0200

    centralized 40

--- src/oemof/thermal_building_model/examples/03_advanced_investment_optimization/08_centralized_supply_multiple_buildings.py
+++ src/oemof/thermal_building_model/examples/03_advanced_investment_optimization/08_centralized_supply_multiple_buildings.py
@@ -782,13 +782,13 @@ def run_main(heat_grid_temperature):
         peak_reference = final_results_ref["Electricity"]["peak_from_grid"]
 
 
-        co2_reduction_factors =  [1,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25,0.2,0.15,0.1,0.05,0.01,-0.01,-0.05,-0.1,-0.2] # [0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.5] [0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
-         #[1,0.9,0.8,0.7,0.6,0.5,0.4][1,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25,0.2,0.15,0.1,0.05]
+        co2_reduction_factors =  [1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1] # [0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.5] [0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
+         #[1,0.9,0.8,0.7,0.6,0.5,0.4][1,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25,0.2,0.15,0.1,0.05,0.01,-0.01,-0.05,-0.1,-0.2]
 
         for co2_reduction_factor in co2_reduction_factors:
             first_co2_run_in_peak_loop = True
-            peak_reduction_factors = [1,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25,0.2,0.15,0.1,0.05]
-
+            peak_reduction_factors = [1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
+            #peak_reduction_factors = [1,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25,0.2,0.15,0.1,0.05]
 
             if co2_reference > 0:
                 co2_new = co2_reference * co2_reduction_factor
@@ -837,7 +837,7 @@ def run_main(heat_grid_temperature):
                         "time": time
                     }
             print("FINISHED PEAK LOOP START SAVING")
-            file_path = "rresults_heat_grid_"+str(heat_grid_temperature)+"_"+str(ueu)+"_" + str(ev) +".pkl"
+            file_path = "rrresults_heat_grid_"+str(heat_grid_temperature)+"_"+str(ueu)+"_" + str(ev) +".pkl"
             if os.path.exists(file_path):
                 # If the file exists, open it and load the data
                 with open(file_path, "rb") as f:
@@ -855,7 +855,7 @@ def run_main(heat_grid_temperature):
             # Save the updated or new results back to the pickle file
             with open(file_path, "wb") as f:
                 pickle.dump(existing_results, f)
-    file_path = "rresults_heat_grid_"+str(heat_grid_temperature)+"_"+str(ueu)+"_" + str(ev) +".pkl"
+    file_path = "rrresults_heat_grid_"+str(heat_grid_temperature)+"_"+str(ueu)+"_" + str(ev) +".pkl"
     if os.path.exists(file_path):
         # If the file exists, open it and load the data
         with open(file_path, "rb") as f:
@@ -875,7 +875,7 @@ def run_main(heat_grid_temperature):
         pickle.dump(existing_results, f)
 
 if __name__ == "__main__":
-    heat_grid_supply_temperatures =[50,40]  # Beispiel #"GEG_standard"
+    heat_grid_supply_temperatures =[40]  # Beispiel #"GEG_standard"
     import multiprocessing
     import os
     for heat_grid_temperature in heat_grid_supply_temperatures:
