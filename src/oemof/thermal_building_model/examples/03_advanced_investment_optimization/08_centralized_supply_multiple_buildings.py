b897e73f6c12f62fc76c2d0609bd39d7f166b487
hill_mx <maximilian.hillen@dlr.de>
2025-09-04 15:31:03 +0200

    centralized 50

--- src/oemof/thermal_building_model/examples/03_advanced_investment_optimization/08_centralized_supply_multiple_buildings.py
+++ src/oemof/thermal_building_model/examples/03_advanced_investment_optimization/08_centralized_supply_multiple_buildings.py
@@ -782,7 +782,7 @@ def run_main(heat_grid_temperature):
         peak_reference = final_results_ref["Electricity"]["peak_from_grid"]
 
         import numpy as np
-        co2_reduction_factors =  np.arange(1, 0.625, -0.125).tolist() # [0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.5] [0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
+        co2_reduction_factors =  np.arange(1, 0.625, -0.12).tolist() # [0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.5] [0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
          #[1,0.9,0.8,0.7,0.6,0.5,0.4][1,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25,0.2,0.15,0.1,0.05,0.01,-0.01,-0.05,-0.1,-0.2]
 
         for co2_reduction_factor in co2_reduction_factors:
