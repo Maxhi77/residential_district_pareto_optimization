2cf600ff877a96b4492a02fddd6373612fe6265a
hill_mx <maximilian.hillen@dlr.de>
2026-01-14 17:56:05 +0100

    add to operation grid possibility off discounting

--- src/oemof/thermal_building_model/input/economics/operation_grid_economics.py
+++ src/oemof/thermal_building_model/input/economics/operation_grid_economics.py
@@ -1,11 +1,11 @@
 from oemof.thermal_building_model.oemof_facades.base_component import GridComponents, PhysicalBaseUnit
-
 # Gas Grid
 bio_gas_grid_config = GridComponents(
     working_rate=0.197 / 1000 * PhysicalBaseUnit.factor, #biogas
     revenue=0,
     price_change_factor=0.0,
     co2_per_flow=0.1297 / 1000 * PhysicalBaseUnit.factor)
+
 # Gas Grid
 natural_gas_grid_config = GridComponents(
     working_rate=0.143 / 1000 * PhysicalBaseUnit.factor, #biogas
@@ -20,7 +20,6 @@ hydrogen_grid_config = GridComponents(
     price_change_factor=0.0,
     co2_per_flow=0.0315 / 1000 * PhysicalBaseUnit.factor)
 
-
 # Electricity Grid
 electricity_grid_config = GridComponents(
     working_rate=0.3393 / 1000 * PhysicalBaseUnit.factor,
@@ -28,13 +27,11 @@ electricity_grid_config = GridComponents(
     price_change_factor=0.0,
     co2_per_flow=0.0783 / 1000 * PhysicalBaseUnit.factor)
 
-# Pellets Grid
-
 pellets_grid_config = GridComponents(
-    working_rate=0.1432 / 1000 * PhysicalBaseUnit.factor,
-    revenue=0.0 / 1000,
-    price_change_factor=0.02,
-    co2_per_flow=0.1655 / 1000 * PhysicalBaseUnit.factor)
+        working_rate=0.1432 / 1000 * PhysicalBaseUnit.factor,
+        revenue=0.0 / 1000,
+        price_change_factor=0.02,
+        co2_per_flow=0.1655 / 1000 * PhysicalBaseUnit.factor)
 
 # Heat Grid
 heat_grid_config = GridComponents(
@@ -42,4 +39,3 @@ heat_grid_config = GridComponents(
     revenue=0.0 / 1000,
     price_change_factor=0.02,
     co2_per_flow=0.1655 / 1000 * PhysicalBaseUnit.factor)
-
