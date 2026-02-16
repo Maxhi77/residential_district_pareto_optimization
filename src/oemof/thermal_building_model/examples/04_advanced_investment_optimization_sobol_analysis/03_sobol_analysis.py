21c0b3c9973b053c84836b2a6e07a0e6a218fdb9
hill_mx <maximilian.hillen@dlr.de>
2026-02-16 12:17:13 +0100

    extend sobol anaylsis, so it includes peak and co2 reducition

--- src/oemof/thermal_building_model/examples/04_advanced_investment_optimization_sobol_analysis/03_sobol_analysis.py
+++ src/oemof/thermal_building_model/examples/04_advanced_investment_optimization_sobol_analysis/03_sobol_analysis.py
@@ -490,15 +490,7 @@ def run_multiprocessing(gap_starter,
             result_key = format_household_key(chosen_household)
             #demand_path = fr'C:\Users\hill_mx\PycharmeProjects\thermal_building_model\src\oemof\thermal_building_model\examples\04_advanced_investment_optimization_sobol_analysis\lpg_profiles\{result_key}'
             demand_path = f'/home/hill_mx/thermal_building_clone/src/oemof/thermal_building_model/examples/04_advanced_investment_optimization_sobol_analysis/lpg_profiles/{result_key}'
-        main(year_of_construction,
-             target_residents,
-             tabula_building_code,
-             building_type,
-             building_size,
-             demand_path,
-             floor_to_roof_area_ratio,
-             azimuth, tilt
-                 )
+
 
         final_results, co2  = main(year_of_construction,
              target_residents,
